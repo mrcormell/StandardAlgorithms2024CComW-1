19ec4da4d81e355917f7a7591214e8b184b624be
David Cormell <d.cormell@etoncollege.org.uk>
2024-11-05 10:05:36 +0000

    Initial Commit

---
+++ StandardAlgorithms/StandardAlgorithmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandardAlgorithmsApp.swift
+//  StandardAlgorithms
+//
+//  Created by admin on 05/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct StandardAlgorithmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
